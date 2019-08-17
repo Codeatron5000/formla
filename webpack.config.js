ead0e4335b61b179beb6489d9597e9e1674eca1f
Chris <cdstarling@outlook.com>
2019-08-17 03:42:12 -0700

    Add bundling

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const webpack = require('webpack');
+const path = require('path');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+function generateConfig(name) {
+    const uglify = name.indexOf('min') > -1;
+    const config = {
+        entry: './src/Form.js',
+        output: {
+            path: path.resolve(__dirname, 'lib'),
+            filename: name + '.js',
+            sourceMapFilename: name + '.map',
+            library: 'formla',
+        },
+        node: {
+            process: false
+        },
+        mode: 'production',
+        optimization: {
+            minimize: uglify,
+        },
+        devtool: 'source-map',
+        module: {
+            rules: [
+                {
+                    test: /\.m?js$/,
+                    exclude: /(node_modules|bower_components)/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            plugins: [
+                                '@babel/plugin-proposal-class-properties',
+                            ],
+                            presets: [
+                                '@babel/preset-flow',
+                            ],
+                        }
+                    }
+                }
+            ]
+        }
+    };
+
+    if (uglify) {
+        config.optimization.minimizer = [new UglifyJsPlugin({
+            sourceMap: true,
+        })];
+    }
+
+    return config;
+}
+
+module.exports = ['Form', 'Form.min'].map(function (key) {
+    return generateConfig(key);
+});
